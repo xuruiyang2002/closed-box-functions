f6d0118c89be1858329c5d8dd14bb9104323e906
xuruiyang2002 <xury5520@outlook.com>
2024-02-02 22:47:18 +0800

    Comparison: memcmp - Compare two blocks of memory (function) strcmp - Compare two strings (function) strcoll - Compare two strings using locale (function) strncmp - Compare characters of two strings (function) strxfrm - Transform string using locale (function)

---
+++ test_strncmp.c
@@ -0,0 +1,23 @@
+#include "klee/klee.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[2];
+
+  klee_make_symbolic(str, sizeof(str), "array");
+  klee_assume(str[1] == '\0');
+
+  // CHECK-DAG: Equal
+  // CHECK-DAG: Greater 
+  // CHECK-DAG: Less 
+  int r = strncmp(str, "e", 1);
+  if (r == 0) printf("Equal\n");
+  else if (r > 0) printf("Greater\n");
+  else printf("Less\n");
+
+  // CHECK-DAG: KLEE: done: completed paths = 3
+  
+  return 0;
+}
+
---
+++ test_strxfrm.c
@@ -0,0 +1,22 @@
+#include "klee/klee.h"
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char str[2];
+
+  klee_make_symbolic(str, sizeof(str), "array");
+  klee_assume(str[1] == '\0');
+
+  // CHECK-DAG: Zero
+  // CHECK-DAG: One 
+  int n = strxfrm(NULL, str, 0); // retrieve the length
+  if (r == 0) printf("Zero\n");
+  else if (r == 1) printf("One\n");
+  else klee_assert(0 && "sanity check\n");
+
+  // CHECK-DAG: KLEE: done: completed paths = 2
+  
+  return 0;
+}
+
