44b7d7baf1c6a3b8ae93e04a76599f8d158d1a52
xuruiyang2002 <xury5520@outlook.com>
2024-02-16 10:39:17 +0800

    Complete the section of cstring that involves copying

---
+++ cstring/test_strcpy.c
@@ -0,0 +1,25 @@
+#include "klee/klee.h"
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+  char src[2], dest[2];
+
+  klee_make_symbolic(src, sizeof(src), "array");
+  klee_assume(src[1] == '\0');
+
+  // CHECK-DAG: Yes
+  // CHECK-DAG: No
+  strcpy(dest, src);
+  if (dest[0] == 'e') {
+    // str[0] is 'e'
+    printf ("Yes\n");
+  } else {
+    // str[0] is not 'e'
+    printf ("No\n");
+  }
+
+  // CHECK-DAG: KLEE: done: completed paths = 2
+
+  return 0;
+}
