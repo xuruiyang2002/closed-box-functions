670ba025bd4b6d43eb5587bd96e6664fbb879abb
xuruiyang2002 <xury5520@outlook.com>
2024-03-15 09:26:17 +0800

    Refine the run.sh script

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ INCLUDE_PATH="${RECOLOSSUS_PATH}/klee/include"
 clang -I ${INCLUDE_PATH} -emit-llvm -g -c $1.c -o $1.bc
 
 # Define common KLEE options
-KLEE_OPTIONS="--warnings-only-to-file --max-time=600 --max-fuzz-solver-time=30 --search=bfs -rewrite-equalities=false --external-calls=all"
+KLEE_OPTIONS="--max-time=600 --max-fuzz-solver-time=30 --search=bfs --external-calls=all"
 
 # Invoke KLEE with Recolossus
 ${KLEE_PATH} --recolossus ${KLEE_OPTIONS} $1.bc
