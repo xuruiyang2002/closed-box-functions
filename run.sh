8e5e508e89fc1560e2361ec5bd12f547c2e2d01d
xuruiyang2002 <xury5520@outlook.com>
2024-03-27 15:17:10 +0800

    Add test cases execrpted from klee-float

--- run.sh
+++ run.sh
@@ -9,8 +9,11 @@ INCLUDE_PATH="${RECOLOSSUS_PATH}/klee/include"
 clang -I ${INCLUDE_PATH} -emit-llvm -g -c $1.c -o $1.bc
 
 # Define common KLEE options
-KLEE_OPTIONS="--max-time=600 --max-fuzz-solver-time=30 --search=bfs --external-calls=all"
+KLEE_OPTIONS="--max-time=600 --search=bfs --external-calls=all"
+# KLEE_OPTIONS="--max-time=600 --max-fuzz-solver-time=30 --search=bfs --external-calls=all"
 
 # Invoke KLEE with Recolossus
-${KLEE_PATH} --recolossus ${KLEE_OPTIONS} $1.bc
+# ${KLEE_PATH} --recolossus ${KLEE_OPTIONS} $1.bc
 
+# Invoke KLEE without Recolossus
+${KLEE_PATH} --debug-z3-dump-queries=$1.smt2 ${KLEE_OPTIONS} $1.bc
